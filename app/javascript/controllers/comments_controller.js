09b20f00a3af2c1beaaad4c15d5d84be149df702
David Colby <davidlcolby@gmail.com>
2020-12-05 11:24:16 -0600

    Use new Class API to set animation class

--- app/javascript/controllers/comments_controller.js
+++ app/javascript/controllers/comments_controller.js
@@ -2,6 +2,7 @@ import { Controller } from "stimulus"
 
 export default class extends Controller {
   static targets = ["commentList", "commentBody", "form"]
+  static classes = ["animateIn"]
 
   connect() {
     console.log("We're connected!")
@@ -12,14 +13,14 @@ export default class extends Controller {
     const target = event.target
 
     const newComment = document.createElement("div")
-    newComment.classList.add("fade-in-left")
+    newComment.classList.add(this.animateInClass)
     newComment.innerHTML = xhr.response
 
     this.commentListTarget.prepend(newComment)
     this.commentBodyTarget.value = ''
 
     setTimeout(() => {
-      newComment.classList.remove("fade-in-left")
+      newComment.classList.remove(this.animateInClass)
       newComment.removeAttribute("id")
     }, 600);
   }
